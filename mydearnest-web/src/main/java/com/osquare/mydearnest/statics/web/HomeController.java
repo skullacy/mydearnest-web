9936d77b0f11a8be0ae6c8efedc5191de1d3a806
skullacy <skullacy@gmail.com>
2013-12-08 20:58:01 +0900

    mysql, hibernate 설정
    Signed-off-by: skullacy <skullacy@gmail.com>

--- mydearnest-web/src/main/java/com/osquare/mydearnest/statics/web/HomeController.java
+++ mydearnest-web/src/main/java/com/osquare/mydearnest/statics/web/HomeController.java
@@ -4,12 +4,17 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -19,12 +24,19 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@RequestMapping("")
+	public String index(ModelMap model, @RequestParam(value = "p", required = false) String p, 
+			HttpServletRequest request, HttpServletResponse response, Locale locale) {
+		
 		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info(model.toString());
+		logger.info(request.toString());
+		logger.info(response.toString());
+		logger.info(p);
+		
+//		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+//		response.setHeader("Pragma", "no-cache");
+//		response.setHeader("Expires", "0");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
