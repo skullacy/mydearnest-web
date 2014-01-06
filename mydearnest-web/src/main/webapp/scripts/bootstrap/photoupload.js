ba281b9e176b0cc80a4aa8c91cf9f2017c0a492d
cray0114 <cray0114@gmail.com>
2014-01-06 21:47:55 +0900

    cray 작업

--- mydearnest-web/src/main/webapp/scripts/bootstrap/photoupload.js
+++ mydearnest-web/src/main/webapp/scripts/bootstrap/photoupload.js
@@ -27,7 +27,7 @@ $(function(){
 			
 			
 			
-			var tag = $('.tagResult').children('.tag_ori').clone();
+			var tag = $('.link_input').children('.tag_ori').clone();
 			console.log(tag);
 			tag.removeClass('tag_ori').addClass('tag').data('value', ui.item.value);
 			tag.children('.tag_label').text(ui.item.label);
@@ -41,35 +41,11 @@ $(function(){
 				return false;
 			});
 			
-			$('.tag_'+ui.item.type).append(tag);
+			$('.tag_accesary').append(tag);
 			tag.show();
 			return false;
 		}
 
 	});
-	$("#test").ImageColorPicker({
-		afterColorSelected: function(event, color){
-			var tag = $('.tagResult').children('.tag_ori').clone();
-			console.log(color);
-			console.log(event);
-			console.log(tag);
-			tag.removeClass('tag_ori').addClass('tag').data('value', color).css('background',color).css('color','#eeeeee').css('text-shadow','none');
-			
-			tag.children('.tag_label').text(color);
-			$("#result_test").text(color);
-			tag.children('.position_hidden').removeClass('position_hidden')
-			.attr('name', 'tagColor').attr('value', color);
-		
-			
-			tag.find('a.delete_tag').click(function(){
-				$(this).parent('.tag').remove();
-				return false;
-			});
-			$('.tag_color').append(tag);
-			tag.show();
-			
-			}
-	});
 	
-
 });
