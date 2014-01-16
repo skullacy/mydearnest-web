41871d92e52ef81249bf1423276b493fad21bfeb
skullacy <skullacy@gmail.com>
2014-01-16 15:39:25 +0900

    Signed-off-by: skullacy <skullacy@gmail.com>

--- mydearnest-web/src/main/webapp/scripts/bootstrap/admin.js
+++ mydearnest-web/src/main/webapp/scripts/bootstrap/admin.js
@@ -89,58 +89,54 @@ $(function(){
 		$(this).css('display','none');
 		$('.color-teach').text(massages[color_click]);
 		$("#test").ImageColorPicker({
-			afterColorSelected: function(event, color){
-				$('.color-space').each(function() {
-					if($(this).has('div.tag').length == 0) {
-						insertColorTag($(this), color);
-						if(color_click == 3) {
-							$('.color-teach').text(massages[color_click+1]);
-						}
-						else {
-							findNextPosition();
-						} 
-							
-						console.log(color_click);
-						
-						
-						return false;
-					}
-					
-				});
-				
-				
-				
-				
-				
-				if(false) {
-				if(color_click < 4){
-					
-					console.log(color_click);
-					var tag = $('.tagResult').children('.color-space').clone().children('.tag_ori');
-					tag.parent('.color-space').attr('data-click', color_click );
-					color_click++;
-					console.log(color);
-					console.log(event);
-					console.log(tag);
-					tag.removeClass('tag_ori').addClass('tag').data('value', color).css('background',color).css('color','#eeeeee').css('text-shadow','none');
-					tag.children('.tag_label').text(color);
-					$("#result_test").text(color);
-					tag.children('.position_hidden').removeClass('position_hidden')
-					.attr('name', 'tagColor').attr('value', color+color_click);
-					tag.find('a.delete_tag').click(function(){
-						color_click = tag.parent('.color-space').data('click');
-						console.log(color_click);
-						$('.color-teach').text(massages[color_click]);
-						$(this).parent('.tag').remove();
-						return false;
-						
-					});
-					$('.color-teach').text(massages[color_click]);
-					$('.tag_color').append(tag.parent('.color-space'));
-					tag.show();
-				}
-				}
-			}
+//			afterColorSelected: function(event, color){
+//				$('.color-space').each(function() {
+//					if($(this).has('div.tag').length == 0) {
+//						insertColorTag($(this), color);
+//						if(color_click == 3) {
+//							$('.color-teach').text(massages[color_click+1]);
+//						}
+//						else {
+//							findNextPosition();
+//						} 
+//							
+//						console.log(color_click);
+//						
+//						
+//						return false;
+//					}
+//					
+//				});
+//				
+//				if(false) {
+//				if(color_click < 4){
+//					
+//					console.log(color_click);
+//					var tag = $('.tagResult').children('.color-space').clone().children('.tag_ori');
+//					tag.parent('.color-space').attr('data-click', color_click );
+//					color_click++;
+//					console.log(color);
+//					console.log(event);
+//					console.log(tag);
+//					tag.removeClass('tag_ori').addClass('tag').data('value', color).css('background',color).css('color','#eeeeee').css('text-shadow','none');
+//					tag.children('.tag_label').text(color);
+//					$("#result_test").text(color);
+//					tag.children('.position_hidden').removeClass('position_hidden')
+//					.attr('name', 'tagColor').attr('value', color+color_click);
+//					tag.find('a.delete_tag').click(function(){
+//						color_click = tag.parent('.color-space').data('click');
+//						console.log(color_click);
+//						$('.color-teach').text(massages[color_click]);
+//						$(this).parent('.tag').remove();
+//						return false;
+//						
+//					});
+//					$('.color-teach').text(massages[color_click]);
+//					$('.tag_color').append(tag.parent('.color-space'));
+//					tag.show();
+//				}
+//				}
+//			}
 		});
 		$('body').mousemove(function(e){
 			
