61d54e9c312b358def499c367b76ec058db87789
Jude <Gyumin@osquareJude>
2014-01-24 15:07:43 +0900

    #78 이슈에 의해 잘못저장된 데이터 찾아서 변경하는 파트.  - /test/posts로 접속하여 잘못저장된 포스트set을 가져온 뒤, 직접입력으로 변경한다.
    Signed-off-by: Jude <Gyumin@osquareJude>

---
+++ mydearnest-web/src/main/java/com/osquare/mydearnest/test/service/ColorTagUpdate.java
@@ -0,0 +1,9 @@
+package com.osquare.mydearnest.test.service;
+
+import java.util.List;
+
+import com.osquare.mydearnest.entity.PostTag;
+
+public interface ColorTagUpdate {
+	List<PostTag> getWrongColorTags();
+}
--- mydearnest-web/src/main/java/com/osquare/mydearnest/test/web/TestController.java
+++ mydearnest-web/src/main/java/com/osquare/mydearnest/test/web/TestController.java
@@ -3,16 +3,18 @@ package com.osquare.mydearnest.test.web;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import net.sf.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -27,15 +29,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.junglebird.webframe.common.PropertiesManager;
 import com.junglebird.webframe.vo.SignedDetails;
 import com.osquare.mydearnest.account.service.AccountService;
+import com.osquare.mydearnest.admin.service.AdminTagCateService;
 import com.osquare.mydearnest.entity.Account;
 import com.osquare.mydearnest.entity.Post;
-import com.osquare.mydearnest.entity.PostGrade;
-import com.osquare.mydearnest.entity.PostUserGrade;
+import com.osquare.mydearnest.entity.PostTag;
+import com.osquare.mydearnest.entity.TagCategory;
 import com.osquare.mydearnest.post.service.PostService;
-import com.osquare.mydearnest.post.validator.PostGradeValidator;
 import com.osquare.mydearnest.post.vo.PostVO;
+import com.osquare.mydearnest.test.service.ColorTagUpdate;
+import com.osquare.mydearnest.util.DetailModifyStatus;
 import com.osquare.mydearnest.util.image.dominant.DominantColor;
 import com.osquare.mydearnest.util.image.dominant.DominantColors;
 
@@ -45,6 +50,9 @@ public class TestController {
 
 	@Autowired private PostService postService;
 	@Autowired private AccountService accountService;
+	@Autowired private ColorTagUpdate colorTagUpdate;
+	@Autowired private AdminTagCateService adminTagCateService;
+	@Autowired private PropertiesManager pm;
 	
 	public static final double minDiff1 = 0.1;
 	public static final double minDiff2 = 0.9;
@@ -82,7 +90,7 @@ public class TestController {
 	 * @brief
 	 * 사진 평가 입력 화면
 	 */
-	@RequestMapping(value = "/grade/{postId}", method = RequestMethod.GET)
+/*	@RequestMapping(value = "/grade/{postId}", method = RequestMethod.GET)
 	public String insertPostUserGrade(Model model, HttpServletRequest request, HttpServletResponse response,
 			@PathVariable("postId") long postId) {
 		
@@ -119,15 +127,135 @@ public class TestController {
 		model.addAttribute("layout", "./shared/layout.admin.vm");
 		
 		return "test/grade";
-	}
+	}*/
 	
 	/**
 	 * @brief
 	 * 사진 평가 입력 처리
 	 */
-	@RequestMapping(value = "/grade/{postId}", method = RequestMethod.POST)
+/*	@RequestMapping(value = "/grade/{postId}", method = RequestMethod.POST)
 	public String insertPostUserGrade(Model model, HttpServletRequest request, HttpServletResponse response,
-			@RequestParam(value = "pagetype", required = false) String pageType,
+			@PathVariable("postId") Long postId,
+			@RequestBody List<Integer> postUserGradeList,
+			BindingResult result) {
+		
+		model.addAttribute("success", false);
+		
+		Authentication authentication = ((SecurityContext) SecurityContextHolder.getContext()).getAuthentication();
+		if (!(authentication.getPrincipal() instanceof SignedDetails)) return "shared/required.login";
+
+		SignedDetails principal = (SignedDetails) authentication.getPrincipal();
+		Account account = accountService.findAccountById(principal.getAccountId());
+		
+		Post post = postService.getPostById(postId);
+
+		model.addAttribute("account", account);
+
+		if (result.hasErrors()) {
+			System.out.println(result.getAllErrors());
+			model.addAttribute("errors", result.getAllErrors());
+		}
+		else {
+			int listSize = postUserGradeList.size();
+			for (int i = 0; i < listSize; i++) {
+				postUserGradeList.get(0)
+			}
+			
+			PostUserGrade postUserGrade = postService.updatePostUserGrade(post, account, postUserGrade1);
+			
+			if (postUserGrade == null) {
+				model.addAttribute("success", false);
+			}
+			else {
+				model.addAttribute("success", true);
+			}
+		}
+
+		return "redirect:/grade/" +  post.getId();
+	}*/
+
+	@RequestMapping(value = "/posts" , method = RequestMethod.GET)
+	public String getWrongColorTags(HttpServletRequest request, HttpServletResponse response) {
+		
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "0");
+		
+		List<PostTag> list = colorTagUpdate.getWrongColorTags();
+		Iterator<PostTag> iterator = list.iterator();
+		Set<Long> postIdSet = new HashSet<Long>();
+		
+		while (iterator.hasNext()) {
+			long postId = iterator.next().getPost().getId();
+			if (iterator.hasNext() && postId == iterator.next().getPost().getId()) {
+				postIdSet.add(postId);				
+			}
+		}
+		System.out.println("Wrong post's id: " + postIdSet);
+		
+		if (postIdSet.isEmpty()) {
+			return "redirect:/";
+		}
+		else {
+			return "redirect:/test/detail/"+postIdSet.iterator().next();			
+		}
+	}
+	
+	@RequestMapping(value = "/detail/{postId}" , method = RequestMethod.GET)
+	public String insertPostDetail(Model model, HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("postId") Long postId) {
+		
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "0");
+		
+		Post post = postService.getPostById(postId);
+		
+		Collection<TagCategory> tagCate = adminTagCateService.getTagCategories();
+		
+		model.addAttribute("tagcate", tagCate);
+		model.addAttribute("post", post);
+		
+		Iterator<PostTag> postTags = post.getPostTag().iterator();
+		boolean postTagFlag = true;
+		
+		while (postTags.hasNext()) {
+			String value = postTags.next().getValue();
+			if (value != null && value.trim() != "") postTagFlag = false;
+		}
+		
+		if (postTagFlag) {
+			BufferedImage bufferedImage = null;
+			
+			try {
+				bufferedImage = ImageIO.read(new URL(pm.get("web_url")+"/mdn-image/thumb/"+post.getImageSource().getId()+"?w=200&t=ratio"));
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			DominantColor[] colors = DominantColors.getDominantColor(bufferedImage, 3, 0.1);
+			
+			TreeMap<Float, String> hexTree = new TreeMap<Float, String>();
+			List<String> hexes = new ArrayList<String>();
+			int colorsLength = colors.length;
+			for (int i = 0; i < colorsLength; i++) {
+				hexTree.put(colors[i].getPercentage(), colors[i].getRGBHex());
+			}
+			for (int i = 0; i < colorsLength; i++) {
+				hexes.add(hexTree.pollLastEntry().getValue());
+			}
+			
+			model.addAttribute("hexes", hexes);
+		}
+		
+		model.addAttribute("layout", "./shared/layout.admin.vm");
+		
+		return "test/post_detail";
+	}
+	
+	@RequestMapping(value = "/detail/{postId}" , method = RequestMethod.POST)
+	public String updateWorngColorTags(Model model, HttpServletRequest request, HttpServletResponse response,
 			@RequestParam(value = "redirectType", required = false) String redirectType,
 			@PathVariable("postId") Long postId,
 			@ModelAttribute("command") PostVO postVO,
@@ -145,50 +273,23 @@ public class TestController {
 
 		model.addAttribute("account", account);
 		model.addAttribute("command", postVO);
-
-		new PostGradeValidator().validate(postVO, result);
 		
 		if (result.hasErrors()) {
+			System.out.println("Write ERRORaslgdk;jasdl;fkasf");
 			System.out.println(result.getAllErrors());
 			model.addAttribute("errors", result.getAllErrors());
 		}
 		else {
-			PostGrade postGrade = null;
-			if("update".equals(pageType)) {
-				postGrade = postService.updatePostGrade(post, account, postVO);
-			}
-			else {
-				postGrade = postService.createPostGrade(post, account, postVO);
-			}
 			
-			if (postGrade == null) {
+			Post postResult = postService.createPostDetail(post, account, postVO);
+			if ( postResult == null) {
 				model.addAttribute("success", false);
 			}
 			else {
 				model.addAttribute("success", true);
-//				model.addAttribute("redirect_uri", request.getContextPath() + "/write/complete.do");
 			}
 		}
 		
-
-		//Submit 후 리다이렉트 지정되지 않은경우.
-		if(redirectType == null) {
-			
-			//미완료된 포스트중 랜덤 호출.
-			Post redirectPost = postService.getPostByRandom(0, "grade", account);
-			
-			if(redirectPost == null) {
-				return "redirect:/admin";
-			}
-			else {
-				return "redirect:/admin/write/grade/"+redirectPost.getId();
-			}
-			
-		}
-		else {
-			return "redirect:/admin/write/" + redirectType + "/" +  post.getId();
-		}
-		
+		return "redirect:/test/posts";
 	}
-	
 }
